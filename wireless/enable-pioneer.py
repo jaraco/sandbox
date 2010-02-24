8181508ddb51b7fd1036fd6ec11147e80116f083
jaraco@76dc99ec-c13e-774f-a8c6-c1e57a12155e <jaraco@76dc99ec-c13e-774f-a8c6-c1e57a12155e>
2010-02-24 03:40:51 +0000

    Trying to get it to work

--- wireless/enable-pioneer.py
+++ wireless/enable-pioneer.py
@@ -1,2 +1,7 @@
 from comtypes.gen import ADHOCLib
+from comtypes import GUID
+from ctypes import POINTER
 
+mgr = ADHOCLib.IDot11AdHocManager()
+p = POINTER(GUID)()
+mgr.GetIEnumDot11AdHocNetworks(mgr)
