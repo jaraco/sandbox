e22fd2dc9cfd8483b257e0c7a46860da04a7489f
Jason R. Coombs <jaraco@jaraco.com>
2016-01-08 14:45:32 -0500

    Implement is_present_anywhere, to iterate over all possible initial positions.

--- exercise.py
+++ exercise.py
@@ -1,23 +1,44 @@
-def is_present(word, array, visited_indexes=(), position=None):
+import itertools
+
+
+def is_present_anywhere(word, array):
+    """
+    Can the word be found in the two-dimensional array
+    of characters by traversing vertically or horizontally
+    starting at any position.
+    """
+    return any(
+        is_present(word, array, position=pos)
+        for pos in initial_positions(array)
+    )
+
+
+def initial_positions(array):
+    width = len(array)
+    height = len(array[0])
+    return itertools.product(range(width), range(height))
+
+
+def is_present(word, array, position, visited_indexes=()):
     """
     Can the word be found in the two-dimensional array
     by traversing vertically or horizontally starting at
-    position (or all positions if None).
+    position.
     """
     assert isinstance(word, str)
     if word == '':
         return True
     letter = word[0]
-    indexes = matching_indexes(letter, array, visited_indexes, position)
+    indexes = matching_indexes(letter, array, position, visited_indexes)
     if not indexes:
         return False
     return any(
-        is_present(word[:1], array, visited_indexes + (position,), position)
+        is_present(word[:1], array, position, visited_indexes + (position,))
         for position in indexes
     )
 
 
-def matching_indexes(letter, array, visited_indexes, position):
+def matching_indexes(letter, array, position, visited_indexes):
     """
     find indexes into array that match letter adjacent to position
     """
